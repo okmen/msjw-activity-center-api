4ab5b6a6ba7c0241175987fe4744b1a6df7aab94
qinyongbo <qinyongbo@chudaokeji.com>
2017-06-05 16:05:32 +0800

    init project

---
+++ src/main/java/cn/activity/service/IEastReservationService.java
@@ -0,0 +1,14 @@
+package cn.activity.service;
+
+import cn.activity.bean.Person;
+
+/**
+ * 东部预约
+ * @author Mbenben
+ *
+ */
+public interface IEastReservationService {
+	
+	public int getPerson(Person person) throws Exception;
+	
+}
